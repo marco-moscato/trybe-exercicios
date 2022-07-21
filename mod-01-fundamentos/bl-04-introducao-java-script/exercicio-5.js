d0f4adbda82d03e6732494854c133bbdea6c2b5e
Marco Moscato <marcomoscatto@gmail.com>
2022-07-21 13:44:12 -0300

    exercicios 5, 6 e 7

---
+++ mod-01-fundamentos/bl-04-introducao-java-script/exercicio-5.js
@@ -0,0 +1,17 @@
+const a = 90;
+const b = 60;
+const c = 30;
+
+// se a soma dos 3 angulas for igual a 180 então true, senão false
+
+if ((a + b + c) == 180) {
+    console.log(true);
+}
+
+ else if ((a || b || c) < 0) {
+    console.log(false);
+}
+
+else {
+    console.log(false);
+}
