d5f0794fc30a7ae6d8c9b7607a7ead0dcb48927e
Marco Moscato <marcomoscatto@gmail.com>
2022-07-23 16:15:13 -0300

    exercicios dia 4.2 e 4.3

---
+++ mod-01-fundamentos/bl-04-introducao-java-script/dia-4.2-js-arrays-loop-for/exercicio-1-aula-ao-vivo.js
@@ -0,0 +1,18 @@
+/*
+
+/*
+
+1. Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato: A soma de 1 a 50 é: X.
+
+*/
+
+
+let resultado = 0;
+
+for (let i = 1; i <= 50; i +=1) {
+   
+    resultado =+ resultado + i;
+
+}
+
+ console.log('A soma de 1 a 50 é: ' + resultado)
---
+++ mod-01-fundamentos/bl-04-introducao-java-script/dia-4.3-exercicio-4.js
@@ -0,0 +1,11 @@
+const n = 9 ;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
+
+/* 
+Utilizando a lógica apresentada acima, qual deve ser o valor salvo na variável n para que o resultado impresso pelo console.log seja 55?
+*/
