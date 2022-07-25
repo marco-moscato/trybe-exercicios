7213af71fa3d59be6296c83dcb91331a03bdcd0e
Marco Moscato <marcomoscatto@gmail.com>
2022-07-25 19:54:21 -0300

    exercicios bloco 4

---
+++ mod-01-fundamentos/bl-04-introducao-java-script/dia-4.2-js-arrays-loop-for/exercicio-7.js
@@ -0,0 +1,6 @@
+/* 
+
+Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+*/
+
---
+++ mod-01-fundamentos/bl-04-introducao-java-script/dia-4.2-js-arrays-loop-for/exercicio-8.js
@@ -0,0 +1,11 @@
+/*
+
+Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+*/
+
+let array25 = [];
+
+for (index = 0; index < 25; index += 1)
+
+    array25.push()
