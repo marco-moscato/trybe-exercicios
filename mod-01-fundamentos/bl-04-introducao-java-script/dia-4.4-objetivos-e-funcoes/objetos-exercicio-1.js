d2343b5342cd0192714b9a14e9b4c5f062ee0c0a
Marco Moscato <marcomoscatto@gmail.com>
2022-07-25 18:29:02 -0300

    exercicio 1 feito

---
+++ mod-01-fundamentos/bl-04-introducao-java-script/dia-4.4-objetivos-e-funcoes/objetos-exercicio-1.js
@@ -0,0 +1,17 @@
+/*
+
+1 - Crie um objeto player contendo as variáveis listadas abaixo.
+
+*/
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2, 
+        silver: 3,
+    },
+};
+
+console.log(player.name);
---
+++ mod-01-fundamentos/bl-04-introducao-java-script/dia-4.4-objetivos-e-funcoes/script.js
@@ -0,0 +1,12 @@
+let diasDaSemana = {
+    : 'domingo',
+    : 'segunda',
+    : 'terça',
+    : 'quarta',
+    : 'quinta',
+    : 'sexta',
+    : 'sábado',
+    };
+    
+   // diasDaSemana.1; // SyntaxError: Unexpected number
+    console.log(diasDaSemana['1']); // domingo
