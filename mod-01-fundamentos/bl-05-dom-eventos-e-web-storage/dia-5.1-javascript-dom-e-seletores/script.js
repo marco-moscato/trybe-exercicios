5ea8d1f88d071454478152e8f485f3729aa762c1
Marco Moscato <marcomoscatto@gmail.com>
2022-07-28 14:38:10 -0300

    exercicios alterados pos monitoria, a fazer

--- mod-01-fundamentos/bl-05-dom-eventos-e-web-storage/dia-5.1-javascript-dom-e-seletores/script.js
+++ mod-01-fundamentos/bl-05-dom-eventos-e-web-storage/dia-5.1-javascript-dom-e-seletores/script.js
@@ -1,1 +1,35 @@
-document.getElementsByTagName("p")[1].innerText = "Desenvolvedor de software no Mercado Livre";
+ /*
+Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+    - document.getElementById()
+    - document.getElementsByClassName()
+    - document.getElementsByTagName()
+*/
+/*
+ Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+ */
+
+const segundoParagrafo = document.getElementsByTagName('p')[1];
+
+  function mudarTexto (element) {
+    element.innerText = 'Como me vejo daqui a 2 anos';
+ }
+
+ mudartexto(segundoParagrafo);
+ 
+ /*
+ Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+ */
+
+ function newB
+
+ /*Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+ Crie e execute uma função que corrija o texto da tag <h1>.
+ Crie e execute uma função que modifique todo o texto da tag <p> para maiúsculo.
+ Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
+ */
+
+
+
+
+
+
