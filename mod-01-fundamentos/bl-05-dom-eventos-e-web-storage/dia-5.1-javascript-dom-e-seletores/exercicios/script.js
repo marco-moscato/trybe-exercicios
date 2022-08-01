8574e9ab0c4e78b530e3a7f62f77c8c29eea80ef
Marco Moscato <marcomoscatto@gmail.com>
2022-08-01 14:57:45 -0300

    exercicios 5.1

---
+++ mod-01-fundamentos/bl-05-dom-eventos-e-web-storage/dia-5.1-javascript-dom-e-seletores/exercicios/script.js
@@ -0,0 +1,37 @@
+ /*
+        Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+        - document.getElementById()
+        - document.getElementsByClassName()
+        - document.getElementsByTagName()
+ Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)*/
+ function changeText() {
+    let paragraph = document.getElementsByTagName("p")[1];
+    paragraph.innerText = "Estar bem empregado na área de tecnologia";
+  }
+  changeText();
+
+/*
+Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).*/
+function changeColor() {
+    let yellowSquare = document.getElementsByClassName("main-content")[0];
+    yellowSquare.style.background = "rgb(76,164,109)";
+}
+changeColor();
+
+/* Crie e execute uma função que mude a cor do quadrado vermelho para branco.*/
+function changeColor() {
+    let redSquare = document.getElementsByClassName("center-content")[0];
+    redSquare.style.background = "white";
+}
+changeColor();
+
+/* Crie e execute uma função que corrija o texto da tag <h1>.*/
+function correctText() {
+    let correctedText = document.getElementsClassName("title")[0];
+    correctedText.innerText = "Exercício 5.1 - JavaScript";
+}
+correctText();
+
+/*Crie e execute uma função que modifique todo o texto da tag <p> para maiúsculo.
+Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
+  */
